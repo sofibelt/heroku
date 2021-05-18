77c57a8c0d2b6ab43a10347eba25408fe942db55
Ana Sofia Beltran <anasobeltran@gmail.com>
2021-05-17 21:35:37 -0500

    intento de pag web

--- heroku.py
+++ heroku.py
@@ -1,10 +1,7 @@
-from flask import Flask
-
-app=Flask(__name__)
-
+from flask import *
+app = Flask(__name__)
 @app.route('/')
-def home():
-    return 'hello world'
-
-if __name__=='__main__':
-    app.run()
+def message():
+      return "<html><body><h1>Hi, welcome to the website</h1></body></html>"
+if __name__ == '__main__':
+   app.run(debug = True)  
